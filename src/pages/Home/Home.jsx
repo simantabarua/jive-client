9eeac637ec71f887570bddbbd1b626c0e12f2d5c
simantabarua <webdevelopersimanta@gmail.com>
2023-06-09 11:09:39 +0600

    generatejwt

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,22 @@
+import axios from "axios";
 
 const Home = () => {
+  const genToken = () => {
+    axios
+      .post("http://localhost:5000/jwt", { data: "hi" })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-<div>Home</div>
+    <div>
+      <button onClick={genToken} className="btn btn-primary">
+        gen
+      </button>
+    </div>
   );
 };
 
